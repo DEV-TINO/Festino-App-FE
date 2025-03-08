dafd5990fdddff1ac0ccc3f2e35387fa786c54c2
jzongyun <jeong.yun@tukorea.ac.kr>
2025-03-08 20:44:45 +0900

    feat: change instagram link from student council to club association

--- src/components/timetable/TimeTable.vue
+++ src/components/timetable/TimeTable.vue
@@ -39,7 +39,7 @@ const updateActiveTimetable = () => {
 };
 
 const handleClickInstagram = () => {
-  window.open(`https://www.instagram.com/tukorea_25_wind/`, '_blank');
+  window.open(`https://www.instagram.com/22th_yoonseul/`, '_blank');
 };
 
 watch(() => day.value, async () => {
@@ -90,7 +90,7 @@ onMounted(async () => {
       </div>
       <div class="text-center flex flex-col gap-2">
         <p class="text-primary-700 text-xs">자세한 공연 정보가 궁금하다면?</p>
-        <button @click="handleClickInstagram()" class="text-white w-[232px] h-[30px] rounded-full bg-primary-700 text-sm">총학생회 wind 인스타그램 바로가기</button>
+        <button @click="handleClickInstagram()" class="text-white px-4 h-[30px] rounded-full bg-primary-700 text-sm">동아리 연합회 윤슬 인스타그램 바로가기</button>
       </div>
     </div>
   </div>
