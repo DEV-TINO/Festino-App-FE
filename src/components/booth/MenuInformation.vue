15e3f4a1995b6dffab5fc82c3581bbfb1bb6a33b
김민지 <alswlk0306@gmail.com>
2025-03-09 16:12:45 +0900

    style: 메뉴 아이템 이미지 중앙 정렬

--- src/components/booth/MenuInformation.vue
+++ src/components/booth/MenuInformation.vue
@@ -26,7 +26,7 @@ const handleClickItem = (index) => {
   <div v-for="(menu, index) in menuList" :key="index" class="dynamic-padding">
     <div
       @click="handleClickItem(index)"
-      class="w-full p-[13px] rounded-3xl shadow-4xl flex mb-[10px]"
+      class="w-full p-[13px] rounded-3xl shadow-4xl flex mb-[10px] items-center"
       :class="menu.isSoldOut ? 'bg-[#E5E7EB] border border-gray-300' : 'bg-white border border-primary-900-light-68'"
     >
       <img
